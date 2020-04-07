08ef8ee2602d316244a7e433ce044a614aff642b
oanaprf <oannna1234@gmail.com>
2020-04-08 00:58:12 +0300

    configured express

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-console.log('hello');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Header', '*');
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.status(200).json({});
+  }
+});
+
+// eslint-disable-next-line no-console
+app.listen(port, (err) => console.log(err || `server listening on port ${port}`));
