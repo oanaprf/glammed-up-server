56efa3aba4bb91b521bb56e64ed8be2c10b0380f
oanaprf <oannna1234@gmail.com>
2020-04-12 20:37:06 +0300

    added firebase authentication

--- server.js
+++ server.js
@@ -2,17 +2,41 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-require('dotenv/config');
 const mongoose = require('mongoose');
+const firebaseAdmin = require('firebase-admin');
+const getOr = require('lodash/fp/getOr');
+
+require('dotenv/config');
 
 const app = express();
 const port = process.env.PORT;
 
+firebaseAdmin.initializeApp({
+  credential: firebaseAdmin.credential.cert({
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  }),
+  databaseURL: process.env.FIREBASEDB_URI,
+});
+
+const authMiddleware = (req, res, next) => {
+  const authToken = getOr(undefined, ['headers', 'authorization'], req);
+  if (authToken) {
+    firebaseAdmin
+      .auth()
+      .verifyIdToken(authToken)
+      .then(() => next())
+      .catch(() => res.status(403).send('Unauthorized'));
+  } else {
+    res.status(403).send('Unauthorized');
+  }
+};
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
-
-app.get('/', (_, res) => res.json({ ok: 'ok' }));
+app.use(authMiddleware);
 
 mongoose
   .connect(process.env.MONGODB_URI, {
@@ -23,5 +47,7 @@ mongoose
 mongoose.connection.on('error', err => {
   console.error(err);
 });
+
+app.get('/', (_, res) => res.json({ ok: 'ok' }));
 
 app.listen(port, err => console.log(err || `server listening on port ${port}`));
